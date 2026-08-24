f6e2fe0d5d9faf7718a541dbd1628b1b1bd73498
drewvrieze <vriezedrew@gmail.com>
2026-08-24 12:35:05 -0600

    Calculated max, min, and standard deviation.

--- Mean&whatnot.py
+++ Mean&whatnot.py
@@ -1,4 +1,7 @@
 import csv
+import math
+
+#calculates average
 with open('primary-time-series.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     count=0
@@ -12,4 +15,37 @@ with open('primary-time-series.csv', newline='') as csvfile:
         count+=1
     average=tally/(count-1)
     print(f"The mean is {average:.3f}")
-    
+
+#Calculates min and max
+with open('primary-time-series.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    count=0
+    tally=0
+    minimum = average
+    maximum = average
+    for row in spamreader:
+        if count > 0:
+            info = row[1].split(',')
+            discharge = float(info[1])
+            if discharge < minimum:
+                minimum = discharge
+            if discharge > maximum:
+                maximum = discharge
+        count += 1
+    print(f"The maximum is {maximum:.3f}")
+    print(f"The minimum is {minimum:.3f}")
+
+#calculates standard deviation
+with open('primary-time-series.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    count=0
+    tally=0
+    sdcounter = 0
+    for row in spamreader:
+        if count > 0:
+            info = row[1].split(',')
+            discharge = float(info[1])
+            sdcounter += (discharge-average)**2
+        count += 1
+    sd = math.sqrt(sdcounter/(count-2))
+    print(f"The standard deviation is {sd:.3f}")
